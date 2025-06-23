7a8818d36d6b5a537a48e2f9189fe2363b431f43
Meireles-88 <vitomar.meireles@hotmail.com>
2025-06-23 17:37:30 -0300

    feat(instituicao): Cria app e refatora modelos com TipoInstituicao

--- instituicao/admin.py
+++ instituicao/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+# instituicao/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import TipoInstituicao, Instituicao
+
+@admin.register(TipoInstituicao)
+class TipoInstituicaoAdmin(admin.ModelAdmin):
+    """
+    Define a interface de administração para o modelo TipoInstituicao.
+    """
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Instituicao)
+class InstituicaoAdmin(admin.ModelAdmin):
+    """
+    Define a interface de administração para o modelo Instituicao.
+    """
+    list_display = ('__str__', 'municipio', 'uf', 'cnpj')
+    search_fields = ('municipio', 'cnpj', 'tipo__nome')
+    list_filter = ('uf', 'tipo')
+    # autocomplete_fields melhora a seleção do 'tipo' quando há muitos registros.
+    autocomplete_fields = ('tipo',)
